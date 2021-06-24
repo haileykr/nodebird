6880ba2affd8b371813e70205a78062134694a6f
haileykr <haileykmrp@gmail.com>
2021-06-24 09:48:08 +0900

    [Add] Added Bcrypt and SignUp Functionality

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const {User} =require('../models');// index.js에서 db에 User가 담겨 exports되어 접근 가능해 짐!
+
+
 const router = express.Router();
 
 
-router.post('/',async (req,res)=>{ // POST /user/
+router.post('/',async (req,res, next)=>{ // POST /user/
 
-    await User.create({
-        email: req.body.email,
-        nickname: req.body.nickname,
-        password: req.body.password
-    })
-    res.json()
+    try {
+        const exUser = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        })
+        if (exUser) {
+            return res.status(403).send('이미 사용중인 이메일입니다');
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await User.create({
+            email: req.body.email,
+            nickname: req.body.nickname,
+            password: hashedPassword
+        })
+        res.status(201).send('ok');
+    } catch (error){
+        console.error(error);
+        next(error); //status 500 (error from server.)
+    }
 
 });
 
