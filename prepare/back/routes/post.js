7a0425592ddbd4a26c8773f26ec8f5fc6feee74e
haileykr <haileykmrp@gmail.com>
2021-07-04 21:43:08 +0900

    [Update] Updated Front Reducer Image Upload Logics

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -1,7 +1,7 @@
 const express = require("express");
-// const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const multer = require("multer");
 
 const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
