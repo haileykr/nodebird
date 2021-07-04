8a5446d518de7a971c3e485bab8e4ebb39d1e16e
haileykr <haileykmrp@gmail.com>
2021-07-04 13:43:46 +0900

    [Update] Modified back config config.js

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -3,25 +3,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": "study",
-    "password":process.env.DB_PASSWORD,
-    "database": "react_nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "study",
+    password:process.env.DB_PASSWORD,
+    database: "react_nodebird",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "test": {
-    "username": "study",
-    "password":process.env.DB_PASSWORD,
-    "database": "react_nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "study",
+    password:process.env.DB_PASSWORD,
+    database: "react_nodebird",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "study",
-    "password":process.env.DB_PASSWORD,
-    "database": "react_nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: "study",
+    password:process.env.DB_PASSWORD,
+    database: "react_nodebird",
+    host: "127.0.0.1",
+    dialect: "mysql"
   }
 }
