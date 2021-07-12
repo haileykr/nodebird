dd7801d0bb4e5f45f8b45e9232b18031d4554308
haileykr <haileykmrp@gmail.com>
2021-07-12 21:32:17 +0900

    [Update] Fixed book router to meet api response format

--- prepare/back/routes/book.js
+++ prepare/back/routes/book.js
@@ -14,7 +14,7 @@ router.get("/popular", async (req, res, next) => {
       url: `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=${process.env.NYT_API_KEY}`,
     });
     
-    const bookDataOnly =nytBookData.results.books;
+    const bookDataOnly =nytBookData.data.results.books;
 
     res.status(200).send(bookDataOnly);
   } catch (error) {
