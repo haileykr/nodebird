abd5c36185c7d0be1cd110a581361420072f11eb
haileykr <haileykmrp@gmail.com>
2021-07-14 19:48:55 +0900

    [Test] Testing Router Data

--- prepare/back/routes/book.js
+++ prepare/back/routes/book.js
@@ -14,9 +14,9 @@ router.get("/popular", async (req, res, next) => {
       url: `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=${process.env.NYT_API_KEY}`,
     });
     
-    const bookDataOnly =nytBookData.data.results.books;
+    // const bookDataOnly =nytBookData.data.results.books;
 
-    res.status(200).send(bookDataOnly);
+    res.status(200).send(nytBookData);
   } catch (error) {
     console.error(error);
     next(error);
